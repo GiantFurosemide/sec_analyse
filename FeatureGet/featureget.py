d964c35a78d1c7d9ad05a1536cd841f0379e61df
Mokusama <18640057101@163.com>
2019-07-11 09:07:51 +0800

    update feature get

--- FeatureGet/featureget.py
+++ FeatureGet/featureget.py
@@ -30,37 +30,38 @@ def df2feature_vector(df:'pandas.dataframe',maxn:int=1):
     about width:
     https://docs.scipy.org/doc/scipy/reference/generated/scipy.signal.peak_widths.html#scipy.signal.peak_widths
     '''
-    # find peaks,return peaks's index array
-    peaks_found,_ = scipy.signal.find_peaks(df.peak)
-    # pick peaks' <.peak> from df by peaks_found
-    sele_peak=df.peak[peaks_found]
-    # pick max value from pd.Series sele_peak,max顺序为数值降序
-    max_array=sele_peak.values[np.argsort(sele_peak)[-maxn:][::-1]]
-    # make index list
-    index_list=[]
-    for i in max_array:
-        index_list.append(sele_peak.index[sele_peak==i][0])
-    # get prominences:array
-    prominences = scipy.signal.peak_prominences(df.peak,index_list)[0] # prominences has 3 arraies,use first 
-    # calculation about width.
-    # x axis is value .time. But results_half returns indexs of .time,with which we calculate width.
-    # width_coefficient*results_half[0] returns what we see in the plot.
-    index_x_max=df.time.index[df.time==df.time.max()][0]
-    index_x_min=df.time.index[df.time==df.time.min()][0]
-    index_length = index_x_max-index_x_min
-    width_coefficient=(df.time.max()-df.time.min())/index_length
-    
-    # get width:array
-    results_half = scipy.signal.peak_widths(df.peak, index_list, rel_height=0.5)[0]*width_coefficient
-    results_full = scipy.signal.peak_widths(df.peak, index_list, rel_height=1)[0]*width_coefficient
-
-
-    # get retention time list
-    retention_time_list = []
-    for i in index_list:
-        retention_time_list.append(df.time[i])
-    
-    return (index_list,max_array,retention_time_list,prominences,results_half,results_full)
+    try:
+        # find peaks,return peaks's index array
+        peaks_found,_ = scipy.signal.find_peaks(df.peak)
+        # pick peaks' <.peak> from df by peaks_found
+        sele_peak=df.peak[peaks_found]
+        # pick max value from pd.Series sele_peak,max顺序为数值降序
+        max_array=sele_peak.values[np.argsort(sele_peak)[-maxn:][::-1]]
+        # make index list
+        index_list=[]
+        for i in max_array:
+            index_list.append(sele_peak.index[sele_peak==i][0])
+        # get prominences:array
+        prominences = scipy.signal.peak_prominences(df.peak,index_list)[0] # prominences has 3 arraies,use first 
+        # calculation about width.
+        # x axis is value .time. But results_half returns indexs of .time,with which we calculate width.
+        # width_coefficient*results_half[0] returns what we see in the plot.
+        index_x_max=df.time.index[df.time==df.time.max()][0]
+        index_x_min=df.time.index[df.time==df.time.min()][0]
+        index_length = index_x_max-index_x_min
+        width_coefficient=(df.time.max()-df.time.min())/index_length
+        
+        # get width:array
+        results_half = scipy.signal.peak_widths(df.peak, index_list, rel_height=0.5)[0]*width_coefficient
+        results_full = scipy.signal.peak_widths(df.peak, index_list, rel_height=1)[0]*width_coefficient
+        # get retention time list
+        retention_time_list = []
+        for i in index_list:
+            retention_time_list.append(df.time[i])
+        
+        return (index_list,max_array,retention_time_list,prominences,results_half,results_full)
+    except TypeError:
+        return ([])
     
     
 def make_name_feature_list(ss:'df'):
